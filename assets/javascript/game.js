17bc34f99a68de741e0efe2f5e1b2c7d87e62305
JTerry <jessica.terry79@yahoo.com>
2018-08-28 10:13:43 -0500

    established global variables in game.js and worked on layout of game index.html

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+//=== Gobal variables for the Outdoor Adventure Game === //
+
+// array for the alphbet
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var categories;     // array of categories
+var selectedChoice; // selected category choice
+var getClue;        // Get wordClue
+var selectedWord;   // selected word 
+var letterGuess;    // user guessed alphabet letter
+var guessed = [];   // an array of stored guesses
+var strikes;        // wrong guessed letters = strikes before game over
+var counter;        // count correct guessed letters
+var space;          // represents the black spaces '-' for the word
+
+//============= Set up the word game board ==============//
+
+
+
+
+
