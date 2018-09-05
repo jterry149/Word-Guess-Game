49ff521a1e1040b68b2795843ba14ec9592c630a
JTerry <jessica.terry79@yahoo.com>
2018-09-05 18:35:56 -0500

    fixed button error and disabled them during a win or loss. Added the hangman image too

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,16 @@
 //=== Load these features of the game ===/
-
+window.onload = function () {
 //=== Gobal variables for the Marvel Hangman Game === //
     var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t", "u", "v", "w", "x", "y", "z"];
 
-    
-    
-    var chosenCategory;         // Selected catagory
     var characterWord ;         // Selected character word
     var userGuess ;             // User geuss
     var storedGeusses = [ ];    // Store  user geusses array
     var strikes;                // Strikes 
     var counter;                // Count correct geusses of letters
     var space;                  // Number of spaces in word '-'
-    
+    var wins = 0;               // Number of wins
+    var losses = 0;             // Number of losses
 
 //============= Set up the word game board ==============//
 
@@ -22,7 +20,8 @@ var buttons = function()
     myButtons = document.getElementById("buttons");
     letters = document.createElement("ul");
 
-    for (var i = 0; i < alphabet.length; i++) {
+    for (var i = 0; i < alphabet.length; i++) 
+    {
         letters.id = "alphabet";
         list = document.createElement("li");
         list.id = "letter";
@@ -31,7 +30,7 @@ var buttons = function()
         myButtons.appendChild(letters);
         letters.appendChild(list);
     }
-}
+};
 
 // Create geusses unordered list 
 var result = function () 
@@ -39,7 +38,9 @@ var result = function ()
     wordHolder = document.getElementById("currentCharacter");
     correct = document.createElement("ul");
 
-    for (var i = 0; i < characterWord.length; i++) {
+    // A for loop to create the spaces for the random word
+    for (var i = 0; i < characterWord.length; i++) 
+    {
       correct.setAttribute("id", "my-word");
       userGuess = document.createElement("li");
       userGuess.setAttribute("class", "guess");
@@ -57,32 +58,55 @@ var result = function ()
       wordHolder.appendChild(correct);
       correct.appendChild(userGuess);
     }
-  }
+};
   
-  // Show how many strikes you have until game over
-  var showStrikes = function () 
-  {
-    remainStrikes = document.getElementById("remainingStrikes");
+// Show how many strikes you have until game over
+var showStrikes = function () 
+{
+    // Get the elements from the index.html pages
+    var remainStrikes = document.getElementById("remainingStrikes");
+    var lose = document.getElementById("totalLosses");
+    
+    lose.innerHTML = losses;
     remainStrikes.innerHTML = "You have " + strikes + " strikes left";
+
+    // Conditional loop to increment losses and tell user game is over
     if (strikes < 1) 
     {
       remainStrikes.innerHTML = "Game Over";
+      letters.setAttribute("class", "disabled");
+      losses += 1;   
+      console.log(losses); 
     }
+    
+    // For loop to check if stored user guess matches the random word
     for (var i = 0; i < storedGeusses.length; i++) 
     {
-      if (counter + space === storedGeusses.length) 
+      if (counter + space === storedGeusses.length)
       {
-        remainStrikes.innerHTML = "You Win!";
-      }
-    }
-  }
+        remainStrikes.innerHTML = "You Win!"; 
+        letters.setAttribute("class", "disabled");
+      }  
+    } 
+
+    // Get elements from the index.html page
+    var win = document.getElementById("totalWins");
+    win.innerHTML = wins;
+
+    // Conditional loop to increment wins
+    if(storedGeusses.length === (counter + space))
+    {  
+        win.innerHTML = wins++;
+        console.log(wins);
+    } 
+};
 
 // The button click functions for the game
 var check = function () 
 {
     list.onclick = function () 
     {
-    var geuss = (this.innerHTML);
+      var geuss = (this.innerHTML);
       this.setAttribute("class", "active");
       this.onclick = null;
       for (var i = 0; i < characterWord.length; i++) 
@@ -98,13 +122,85 @@ var check = function ()
       {
         strikes -= 1;
         showStrikes();
-        //animate();
+        animate();
       } else 
       {
         showStrikes();
       }
     }
-  }
+};
+  
+ // Start the animation of the hangman character 
+var animate = function () 
+{
+    var drawMe = strikes;
+    drawArray[drawMe](); 
+}
+
+  
+// Draw the canvas hangman
+drawHangman =  function()
+{
+    myHangman = document.getElementById("hangman");
+    context = myHangman.getContext('2d');
+    context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = 2;
+};
+  
+head = function()
+{
+      myHangman = document.getElementById("hangman");
+      context = myHangman.getContext("2d");
+      context.beginPath();
+      context.arc(60, 25, 10, 0, Math.PI*2, true);
+      context.stroke();
+}
+    
+draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
+    
+    context.moveTo($pathFromx, $pathFromy);
+    context.lineTo($pathTox, $pathToy);
+    context.stroke(); 
+}
+
+   frame1 = function() 
+   {
+     draw (0, 150, 150, 150);
+   };
+   
+   frame2 = function() {
+     draw (10, 0, 10, 600);
+   };
+  
+   frame3 = function() {
+     draw (0, 5, 70, 5);
+   };
+  
+   frame4 = function() {
+     draw (60, 5, 60, 15);
+   };
+  
+   torso = function() {
+     draw (60, 36, 60, 70);
+   };
+  
+   rightArm = function() {
+     draw (60, 46, 100, 50);
+   };
+  
+   leftArm = function() {
+     draw (60, 46, 20, 50);
+   };
+  
+   rightLeg = function() {
+     draw (60, 70, 100, 100);
+   };
+  
+   leftLeg = function() {
+     draw (60, 70, 20, 100);
+   };
+drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1];  
 
 // Play the marvel hangman game
 play = function () 
@@ -116,19 +212,29 @@ play = function ()
     word = word.replace(/\s/g, "_");
    
     console.log(characterWord);
-    
+   
     buttons();
     storedGeusses = [ ];
-    strikes = 8;
+    strikes = 10;
     counter = 0;
     space = 0;
+    lose = losses;
+    wins = wins;
     result();
     showStrikes();
-    
-    //hangman();
-  }
+    drawHangman();
+};
+play();
+// Reset the game play
+document.getElementById("reset").onclick = function() 
+{
+    correct.parentNode.removeChild(correct);
+    letters.parentNode.removeChild(letters);
+    context.clearRect(0, 0, 500, 500);
+    play(); 
+}
 
-
+}
 
 
   
